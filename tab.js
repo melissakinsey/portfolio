84379bf319f5426396bb31830b3d3559ce1f7823
Melissa.Kinsey <kinsey.melissa@gmail.com>
2024-01-11 14:50:24 -0600

    feat: Add white tab background to the currently selected menu tab.

---
+++ tab.js
@@ -0,0 +1,13 @@
+<script>
+let btn = document.getElementsByClassName("collapse");
+    btn[0].addEventListener("click", function () {
+        this.classList.toggle("active")}
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none"
+    }
+    else {
+        content.style.display = "block"
+    }
+);
+</script>
